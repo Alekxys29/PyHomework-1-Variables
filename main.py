40d6ff56de7110d60a1791cf3b062be6e44c23b7
exquisiteVisage <vince.doit@nwtf.org.ph>
2019-01-08 14:08:35 +0800

    homework update

---
+++ main.py
@@ -0,0 +1,42 @@
+""" 
+Homework Assignment #1: Variables
+
+This program will print out the different characteristics of a song.
+"""
+
+# Title of the song
+Title = "The Scientist"
+
+# Album where the song is from
+Album = "A Rush of Blood to the Head"
+
+# Artist of the song
+Artist = "Coldplay"
+
+# Genre of the song
+Genre = "Alternative Rock"
+
+# Year the song was recorded
+YearRecorded = 2001
+
+# Date when the song was released
+DateReleased = "November 4, 2002"
+
+# Length of the song in seconds
+DurationInSeconds = 309
+
+# Producer of the song
+Producer = "Ken Nelson"
+
+
+# Print out the variables
+# I used old style formatting to properly format my output
+
+print('Title: %s' % Title)
+print('Album: %s' % Album)
+print('Artist: %s' % Artist)
+print('Genre: %s' % Genre)
+print('Year Recorded: %s' % YearRecorded)
+print('Date Released: %s' % DateReleased)
+print('Duration: %s seconds' % DurationInSeconds)
+print('Producer: %s' % Producer)
